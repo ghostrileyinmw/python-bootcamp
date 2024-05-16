cf4bca10f51196466f8d0067dbcdae3f5c66c724
ghostrileyinmw <ghostrileyinmodernwarfare@gmail.com>
2024-05-16 16:27:18 +0530

    Add files via upload

---
+++ selectio.py
@@ -0,0 +1,5 @@
+message = "Hello, Python!"
+first_letter = message[2]  # Accesses the first character
+last_letter = message[-1]  # Accesses the last character
+print("First letter:", first_letter)
+print("Last letter:", last_letter)
