1651c2fd8d35bb34db8bfb8592f9f98ae9b9af65
ghostrileyinmw <ghostrileyinmodernwarfare@gmail.com>
2024-05-16 16:08:34 +0530

    Add files via upload

---
+++ addtwonumbers.py
@@ -0,0 +1,3 @@
+numone=int(input("enter a number"))
+numtwo=int(input("enter second number"))
+print(numone+numtwo)
---
+++ evenorodd.py
@@ -0,0 +1,5 @@
+usernumber=int(input("enter your number :"))
+if usernumber % 2==0:
+    print("even")
+else:
+    print("odd")
